0a6591474bd442bc7f5da4fe80257eed69c267cb
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-06-01 14:10:15 +0900

    アドオン

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,35 @@
-import type { StorybookConfig } from "@storybook/nextjs";
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  stories: [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  framework: {
-    name: "@storybook/nextjs",
-    options: {},
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-postcss',
+  ],
+  staticDirs: ['public'],
+  babel: async options => ({
+    ...options,
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.resolve.plugins = [
+      new TsconfigPathsPlugin({
+        configFile: path.resolve(__dirname, '../tsconfig.json')
+      }),
+    ];
+    return config
   },
-  docs: {
-    autodocs: "tag",
+  core: {
+    builder: 'webpack5',
   },
-  staticDirs: ["public"],
-};
-export default config;
+  typescript : { reactDocgen: false},
+}
