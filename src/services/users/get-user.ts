a5525d3e9799052fa88c80cb8983d5bed1fa673c
t4jet <mi201334@edu.okinawa-ct.ac.jp>
2023-06-06 11:13:44 +0900

    Api client

---
+++ src/services/users/get-user.ts
@@ -0,0 +1,21 @@
+import type { ApiContext, User } from "@/types/data"
+import { fetcher } from '../../utils'
+
+export type GetUserParams = {
+  id: number
+}
+const getUser = async(
+  context: ApiContext,
+  { id }: GetUserParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/users/${id}`,
+    {
+      headers: {
+        Accept: 'application/jsno',
+        'Contetn-Type': 'application/json',
+      },
+    },
+  )
+}
+export default getUser
